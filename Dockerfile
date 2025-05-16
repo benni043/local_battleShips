6040d7189c94db554ac71db2aeb9b608eaf339ec
benni <benni.huff@gmail.com>
2025-05-16 23:10:29 +0200

    asd

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,18 @@
-FROM ubuntu:latest
-LABEL authors="benni"
+# 1. Basis-Image
+FROM node:18
 
-ENTRYPOINT ["top", "-b"]
+  # 2. Arbeitsverzeichnis im Container
+WORKDIR /app
+
+  # 3. Kopiere package.json und installiere Abhängigkeiten
+COPY package*.json ./
+RUN npm install
+
+  # 4. Kopiere restlichen Code
+COPY . .
+
+  # 5. Exponiere Port 3006
+EXPOSE 3006
+
+  # 6. Startbefehl
+CMD ["node", "server.js"]
---
+++ server/server.js
@@ -0,0 +1,24 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json()); // damit JSON gelesen werden kann
+
+// Beispiel-Route für POST
+app.post('/daten', (req, res) => {
+    console.log("Empfangene Daten:", req.body);
+    res.send({ message: "Daten erhalten", empfangen: req.body });
+});
+
+// Test-Route für GET
+app.get('/', (req, res) => {
+    res.send("Server läuft!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server läuft auf http://localhost:${PORT}`);
+});
